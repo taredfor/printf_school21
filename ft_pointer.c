e3c3d2939e8ab8b870dc0709132f16e104115aa1
taredfor <taredfor@student.21-school.ru>
2021-06-20 22:23:06 +0300

    create ft_pointer

--- ft_pointer.c
+++ ft_pointer.c
@@ -16,9 +16,17 @@ void	ft_printing_pointer(t_flag *s_pec, unsigned long x)
 			{
 				ft_putchar_len('0', s_pec);
 				ft_putchar_len('x', s_pec);
-				ft_putchar_len('x', s_pec);
+				ft_zero_print(s_pec, s_pec->width, 0);
 			}
 		}
+		return ;
 	}
-
+	if (s_pec->precision > -1)
+		s_pec->zero = 0;
+	s_pec->base = 16;
+	str = ft_itoa(x, s_pec);
+	if (s_pec->precision <= ft_strlen(str))
+		s_pec->precision = ft_strlen(str);
+	if (!s_pec->minus)
+		ft_zero_print(s_pec, s_pec->width - s_pec->precision - 1, 0);
 }
